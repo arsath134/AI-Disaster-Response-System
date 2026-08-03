aca92637f811367ba325c839d57586e2b8b19214
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 14:32:31 +0530

    Update lambda_function.py

--- lambda/incident_processor/lambda_function.py
+++ lambda/incident_processor/lambda_function.py
@@ -4,90 +4,141 @@ import urllib.parse
 from datetime import datetime
 
 
-s3 = boto3.client("s3")
+from services.ai_service import generate_ai_response
 
-dynamodb = boto3.resource(
+
+
+s3=boto3.client(
+    "s3"
+)
+
+
+
+dynamodb=boto3.resource(
     "dynamodb",
     region_name="ap-southeast-2"
 )
 
-table = dynamodb.Table(
+
+
+table=dynamodb.Table(
     "incident-analysis"
 )
 
 
 
-def analyze_disaster(description):
 
-    text = description.lower()
-
-    if "fire" in text:
-        return "CRITICAL"
-
-    elif "flood" in text:
-        return "HIGH"
-
-    elif "earthquake" in text:
-        return "CRITICAL"
-
-    elif "storm" in text:
-        return "HIGH"
-
-    else:
-        return "LOW"
+def lambda_handler(event,context):
 
 
-
-def lambda_handler(event, context):
-
-    record = event["Records"][0]
+    print("EVENT:")
+    print(json.dumps(event))
 
 
-    bucket = record["s3"]["bucket"]["name"]
+    record=event["Records"][0]
 
-    key = urllib.parse.unquote_plus(
+
+    bucket=record["s3"]["bucket"]["name"]
+
+
+    key=urllib.parse.unquote_plus(
+
         record["s3"]["object"]["key"]
+
     )
 
 
-    response = s3.get_object(
+
+    print("Bucket:",bucket)
+
+    print("File:",key)
+
+
+
+    response=s3.get_object(
+
         Bucket=bucket,
+
         Key=key
+
     )
 
 
-    report = json.loads(
+    report=json.loads(
+
         response["Body"].read()
+
     )
 
 
-    severity = analyze_disaster(
-        report["description"]
-    )
+    print("REPORT:")
+    print(report)
 
 
-    item = {
 
-        "report": key,
+    ai_result=generate_ai_response(report)
 
-        "location": report["location"],
 
-        "type": report["type"],
 
-        "description": report["description"],
+    print("AI RESULT:")
+    print(ai_result)
 
-        "severity": severity,
 
-        "status": "OPEN",
 
-        "time": datetime.now().isoformat()
+    item={
+
+
+        "report":key,
+
+
+        "location":
+        report["location"],
+
+
+
+        "type":
+        report["type"],
+
+
+
+        "description":
+        report["description"],
+
+
+
+        "severity":
+        ai_result["severity"],
+
+
+
+        "first_person":
+        ai_result["first_person_instruction"],
+
+
+
+        "third_person":
+        ai_result["third_person_instruction"],
+
+
+
+        "status":
+        "OPEN",
+
+
+
+        "time":
+        datetime.now().isoformat()
 
     }
 
 
+
     table.put_item(
+
         Item=item
+
     )
+
 
 
     return {
