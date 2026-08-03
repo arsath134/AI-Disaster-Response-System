7729e5205e3c298546a0c450fa06396d9942a65d
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 10:20:33 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,17 @@
+import os
 
+class Config:
+
+    AWS_REGION = "ap-southeast-2"
+
+    S3_BUCKET = "ai-disaster-uploads"
+
+    DYNAMODB_TABLE = "IncidentReports"
+
+    SNS_TOPIC_ARN = "YOUR_SNS_TOPIC_ARN"
+
+    AI_PROVIDER = "groq"
+
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+    MAX_UPLOAD_SIZE = 20 * 1024 * 1024
