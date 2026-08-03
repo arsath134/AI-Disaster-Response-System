dcecfd197b4b956324676fa7795f9b799b744819
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 12:53:31 +0530

    Add S3 upload_report function using boto3

--- services/s3_service.py
+++ services/s3_service.py
@@ -1,1 +1,21 @@
+import boto3
 
+
+s3=boto3.client(
+"s3"
+)
+
+
+
+def upload_report(bucket,key,data):
+
+
+    s3.put_object(
+
+        Bucket=bucket,
+
+        Key=key,
+
+        Body=data
+
+    )
