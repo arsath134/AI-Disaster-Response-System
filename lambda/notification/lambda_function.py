a53f7bdf740b9f8acf21c2d603bd5e1aac3ce33a
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 13:40:28 +0530

    Update lambda_function.py

--- lambda/notification/lambda_function.py
+++ lambda/notification/lambda_function.py
@@ -1,33 +1,60 @@
 import boto3
 
 
-sns=boto3.client(
-"sns"
+sns = boto3.client(
+    "sns",
+    region_name="ap-southeast-2"
 )
 
 
-
-TOPIC_ARN="YOUR_TOPIC_ARN"
-
+SNS_TOPIC_ARN = "YOUR_SNS_TOPIC_ARN"
 
 
-def lambda_handler(event,context):
+def lambda_handler(event, context):
+
+    for record in event["Records"]:
+
+        if record["eventName"] == "INSERT":
+
+            data = record["dynamodb"]["NewImage"]
+
+            severity = data["severity"]["S"]
 
 
-    sns.publish(
+            if severity in ["HIGH", "CRITICAL"]:
 
-        TopicArn=TOPIC_ARN,
+                message = f"""
+🚨 DISASTER ALERT 🚨
 
-        Message=
-        "Emergency Disaster Alert Generated"
+Location:
+{data['location']['S']}
 
-    )
+Type:
+{data['type']['S']}
+
+Severity:
+{severity}
+
+Description:
+{data['description']['S']}
+"""
+
+
+                sns.publish(
+
+                    TopicArn=SNS_TOPIC_ARN,
+
+                    Subject="Emergency Disaster Alert",
+
+                    Message=message
+
+                )
 
 
     return {
 
+        "statusCode": 200,
 
-        "status":
-        "Notification Sent"
+        "body": "Notification completed"
 
     }
