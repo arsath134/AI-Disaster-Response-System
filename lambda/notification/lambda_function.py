5e36308f32f17ec414d758115dfe3ae66c079740
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 12:51:55 +0530

    Add SNS notification to lambda function

--- lambda/notification/lambda_function.py
+++ lambda/notification/lambda_function.py
@@ -1,1 +1,33 @@
+import boto3
 
+
+sns=boto3.client(
+"sns"
+)
+
+
+
+TOPIC_ARN="YOUR_TOPIC_ARN"
+
+
+
+def lambda_handler(event,context):
+
+
+    sns.publish(
+
+        TopicArn=TOPIC_ARN,
+
+        Message=
+        "Emergency Disaster Alert Generated"
+
+    )
+
+
+    return {
+
+
+        "status":
+        "Notification Sent"
+
+    }
