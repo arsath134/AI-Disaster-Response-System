0d4744dc80d0ab4533e200e72e2758b785e89a44
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 12:53:02 +0530

    Add save_incident function to save data to DynamoDB

--- services/dynamodb_service.py
+++ services/dynamodb_service.py
@@ -1,1 +1,22 @@
+import boto3
 
+
+dynamodb=boto3.resource(
+"dynamodb"
+)
+
+
+
+def save_incident(data):
+
+
+    table=dynamodb.Table(
+    "incident-analysis"
+    )
+
+
+    table.put_item(
+
+        Item=data
+
+    )
