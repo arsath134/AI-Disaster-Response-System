727dfc538982da996cc61a231c0d374357289cd1
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 12:51:32 +0530

    Implement Lambda function for incident processing

--- lambda/incident_processor/lambda_function.py
+++ lambda/incident_processor/lambda_function.py
@@ -1,1 +1,57 @@
+import json
+import boto3
+from datetime import datetime
 
+
+dynamodb=boto3.resource("dynamodb")
+
+
+table=dynamodb.Table(
+"incident-analysis"
+)
+
+
+
+def lambda_handler(event,context):
+
+
+    record=event["Records"][0]
+
+
+    bucket=record["s3"]["bucket"]["name"]
+
+    file=record["s3"]["object"]["key"]
+
+
+    result={
+
+        "report":file,
+
+        "severity":"HIGH",
+
+        "analysis":
+        "Emergency situation detected",
+
+        "time":
+        datetime.now().isoformat()
+
+    }
+
+
+
+    table.put_item(
+
+        Item=result
+
+    )
+
+
+    return {
+
+
+        "statusCode":200,
+
+        "body":
+        json.dumps(result)
+
+    }
