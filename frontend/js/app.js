2fc38bae8e3fd70461a3396c630a5ceb20a3f8d5
arsath134 <147827230+arsath134@users.noreply.github.com>
2026-08-03 12:51:07 +0530

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,78 @@
+AWS.config.update({
 
+region:"ap-southeast-2"
+
+});
+
+
+const bucketName="YOUR_BUCKET_NAME";
+
+
+const s3=new AWS.S3();
+
+
+
+function submitReport(){
+
+
+let report={
+
+
+id:Date.now().toString(),
+
+location:
+document.getElementById("location").value,
+
+
+type:
+document.getElementById("type").value,
+
+
+description:
+document.getElementById("description").value,
+
+
+time:
+new Date().toISOString()
+
+
+};
+
+
+
+s3.putObject({
+
+Bucket:bucketName,
+
+Key:"reports/"+report.id+".json",
+
+Body:JSON.stringify(report),
+
+ContentType:"application/json"
+
+
+},function(error,data){
+
+
+if(error){
+
+alert(error);
+
+}
+
+else{
+
+
+alert(
+"Report submitted successfully"
+);
+
+
+}
+
+
+
+});
+
+
+}
